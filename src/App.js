e21f5ba58d66f9e6a0d9cb0e23efd2f763d0cd6e
Racenna <mrvader228@gmail.com>
2020-07-17 18:20:08 +0300

    Tried React.lazy and React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { compose } from "redux";
@@ -12,10 +12,14 @@ import NavbarContainer from "./components/Navbar/NavbarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginContainer from "./components/Login/LoginContainer";
+// import LoginContainer from "./components/Login/LoginContainer";
 import Preloader from "./components/common/Preloader/Preloader";
 // Style
 import "./App.css";
+// React.lazy
+const LoginContainer = React.lazy(() =>
+  import("./components/Login/LoginContainer")
+);
 
 class App extends React.Component {
   componentDidMount() {
@@ -40,7 +44,14 @@ class App extends React.Component {
             <Route path="/news" render={News} />
             <Route path="/musics" render={Musics} />
             <Route path="/settings" render={Settings} />
-            <Route path="/login" render={() => <LoginContainer />} />
+            <Route
+              path="/login"
+              render={() => (
+                <Suspense fallback={<div>Loading...</div>}>
+                  <LoginContainer />
+                </Suspense>
+              )}
+            />
           </div>
         </div>
       );
