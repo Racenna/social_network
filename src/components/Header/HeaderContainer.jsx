f7dabb34127f557384fa5b81a1b62729e088a452
Racenna <mrvader228@gmail.com>
2021-01-07 19:40:12 +0200

    Update HeaderContainer.jsx
    HeaderContainer изменена на функциональную.

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -3,11 +3,9 @@ import { connect } from 'react-redux';
 import { logout } from '../../redux/authReducer';
 import Header from './Header';
 
-class HeaderContainer extends React.Component {
-  render() {
-    return <Header {...this.props} />;
-  }
-}
+const HeaderContainer = (props) => {
+  return <Header {...props} />;
+};
 
 const mapStateToProps = (state) => ({
   isAuth: state.auth.isAuth,
