b0dab9a9449dc87b989593c126aa36b39184e301
Racenna <mrvader228@gmail.com>
2021-01-07 21:08:17 +0200

    Update App.js
    App изменена на функциональную и использует хуки.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
@@ -21,55 +21,49 @@ const LoginContainer = React.lazy(() =>
   import('./components/Login/LoginContainer')
 );
 
-class App extends React.Component {
-  catchAllUnhandledErrors = (reason, promise) => {
-    console.error('MY ERROR:', reason);
-  };
+const App = (props) => {
+  const { initialized, initializeApp } = props;
 
-  componentDidMount() {
-    this.props.initializeApp();
-    window.addEventListener('unhandledrejection', this.catchAllUnhandledErrors);
-  }
+  useEffect(() => {
+    function catchAllUnhandledErrors(reason, promise) {
+      console.error('My error:', reason);
+    }
 
-  componentWillUnmount() {
-    window.removeEventListener(
-      'unhandledrejection',
-      this.catchAllUnhandledErrors
+    initializeApp();
+    window.addEventListener('unhandledrejection', catchAllUnhandledErrors);
+    return function cleanup() {
+      window.removeEventListener('unhandledrejection', catchAllUnhandledErrors);
+    };
+  }, [initializeApp]);
+
+  if (!initialized) {
+    return <Preloader />;
+  } else {
+    return (
+      <div className='app-wrapper'>
+        <HeaderContainer />
+        <NavbarContainer />
+        <div className='app-wrapper-content'>
+          <Route path='/' render={() => <Redirect to='/profile' />} />
+          <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
+          <Route path='/dialogs' render={() => <DialogsContainer />} />
+          <Route path='/users' render={() => <UsersContainer />} />
+          <Route path='/news' render={News} />
+          <Route path='/musics' render={Musics} />
+          <Route path='/settings' render={Settings} />
+          <Route
+            path='/login'
+            render={() => (
+              <Suspense fallback={<div>Loading...</div>}>
+                <LoginContainer />
+              </Suspense>
+            )}
+          />
+        </div>
+      </div>
     );
   }
-
-  render() {
-    if (!this.props.initialized) {
-      return <Preloader />;
-    } else
-      return (
-        <div className='app-wrapper'>
-          <HeaderContainer />
-          <NavbarContainer />
-          <div className='app-wrapper-content'>
-            <Route path='/' render={() => <Redirect to='/profile' />} />
-            <Route
-              path='/profile/:userId?'
-              render={() => <ProfileContainer />}
-            />
-            <Route path='/dialogs' render={() => <DialogsContainer />} />
-            <Route path='/users' render={() => <UsersContainer />} />
-            <Route path='/news' render={News} />
-            <Route path='/musics' render={Musics} />
-            <Route path='/settings' render={Settings} />
-            <Route
-              path='/login'
-              render={() => (
-                <Suspense fallback={<div>Loading...</div>}>
-                  <LoginContainer />
-                </Suspense>
-              )}
-            />
-          </div>
-        </div>
-      );
-  }
-}
+};
 
 const mapStateToProps = (state) => ({
   initialized: state.app.initialized,
