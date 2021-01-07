a1bd9ef958557d9e07cf324a98b195915786368e
Racenna <mrvader228@gmail.com>
2021-01-07 20:06:05 +0200

    Update withAuthRedirect.jsx
    Изменил RedirectComponent на функциональную.

--- src/hoc/withAuthRedirect.jsx
+++ src/hoc/withAuthRedirect.jsx
@@ -7,13 +7,11 @@ const mapStateToPropsRedirect = (state) => ({
 });
 
 export const withAuthRedirect = (Component) => {
-  class RedirectComponent extends React.Component {
-    render() {
-      if (!this.props.isAuth) return <Redirect to='/login' />;
+  const RedirectComponent = (props) => {
+    if (!props.isAuth) return <Redirect to='/login' />;
 
-      return <Component {...this.props} />;
-    }
-  }
+    return <Component {...props} />;
+  };
 
   let ConnectedAuthRedirectComponent = connect(mapStateToPropsRedirect)(
     RedirectComponent
